f412c8436fab760106519e4a6a1dae4c520020d1
Darshan Koirala <theanimatorspal@gmail.com>
2025-04-16 11:52:36 +0545

    ✅ Improved Plots and Model Prediction

--- R/model.R
+++ R/model.R
@@ -30,7 +30,7 @@ model_predict <- function(features,
       ggplot(aes(x = date, y = adjusted))+
         geom_line(color = "black", alpha = 0.1)+
         coord_cartesian(xlim = as.Date(c(date_start, date_end)))+
-        geom_ribbon(aes(ymin = adjusted - 5, ymax = adjusted + 5, fill = predicted == direction), alpha = 0.3)
+        geom_ribbon(aes(ymin = adjusted - 5, ymax = adjusted + 5, fill = predicted == direction), alpha = 0.5)
   } else if (model_type == "xgb") {
     label <- as.numeric(as.character(train_data$direction))
     xgb_dtrain <- xgb.DMatrix(data = train_data_as_matrix, label = label)
@@ -39,20 +39,21 @@ model_predict <- function(features,
                     objective = "binary:logistic", verbose = 0) 
     predicted_ <- predict(model, xgb_dtest)
     test_plot <- test_data %>% 
-      mutate(
-        predicted = cut(as.numeric(predicted_),
-                        breaks = c(0, 0.33, 0.66, 1),
-                        labels = c( "low", "med", "high")),
-        difference = as.numeric(direction) - as.numeric(predicted_)
-      ) %>% 
-      ggplot(aes(x = date, y = adjusted))+
-        geom_line(color = "black", alpha = 0.1)+
-        geom_ribbon(aes(ymin = adjusted - 5,
-                        ymax = adjusted + 5,
-                        fill = abs(difference) < 0.3),
-                        alpha = 0.3)+
-        coord_cartesian(xlim = as.Date(c(date_start, date_end)))+
-        scale_color_manual(values = c("red", "blue"))
+        mutate(
+          predicted = predicted_,
+          predicted_classes = cut(as.numeric(predicted_),
+                          breaks = c(0, 0.33, 0.66, 1),
+                          labels = c( "low", "med", "high")),
+          correct = ifelse(predicted > 0.5, 1, 0) == direction,
+          difference = as.numeric(direction) - as.numeric(predicted_)
+        ) %>% 
+        ggplot(aes(x = date, y = adjusted))+
+          geom_line(color = "black", alpha = 0.1)+
+          coord_cartesian(xlim = as.Date(c(date_start, date_end)))+
+          geom_ribbon(aes(ymin = adjusted - predicted * 10,
+                          ymax = adjusted + predicted * 10,
+                          fill = correct), alpha = 0.5)+
+          scale_color_manual(values = c("red", "blue"))
   } else if (model_type == "lbm") {
     label <- as.numeric(as.character(train_data$direction))
     lgb_dtrain <- lgb.Dataset(data = train_data_as_matrix, label = label)
@@ -63,18 +64,19 @@ model_predict <- function(features,
     predicted_ <- predict(model, test_data_as_matrix)
     test_plot <- test_data %>% 
         mutate(
-          predicted = cut(as.numeric(predicted_),
+          predicted = predicted_,
+          predicted_classes = cut(as.numeric(predicted_),
                           breaks = c(0, 0.33, 0.66, 1),
                           labels = c( "low", "med", "high")),
-        difference = as.numeric(direction) - as.numeric(predicted_)
+          correct = ifelse(predicted > 0.5, 1, 0) == direction,
+          difference = as.numeric(direction) - as.numeric(predicted_)
         ) %>% 
         ggplot(aes(x = date, y = adjusted))+
           geom_line(color = "black", alpha = 0.1)+
           coord_cartesian(xlim = as.Date(c(date_start, date_end)))+
-          geom_ribbon(aes(ymin = adjusted - 5,
-                          ymax = adjusted + 5,
-                          fill = abs(difference) < 0.3),
-                          alpha = 0.3)+
+          geom_ribbon(aes(ymin = adjusted - predicted * 10,
+                          ymax = adjusted + predicted * 10,
+                          fill = correct), alpha = 0.5)+
           scale_color_manual(values = c("red", "blue"))
       
   }
@@ -82,31 +84,35 @@ model_predict <- function(features,
   return (list(model, test_plot))   
 } 
 
-data <- get_stock_data()
-features <- generate_features(data) %>% 
-  mutate(
-    direction = factor(ifelse(returns > 0, 1, 0))
-  )
-
-train_data <- features %>% 
-  filter(date <= as.Date("2023-1-1"))
-test_data <- features %>% 
-  filter(date > as.Date("2023-1-1"))
-
-prediction_rf <- model_predict(features,
-                               train_data,
-                               test_data,
-                               model_type = "rf")
-
-
-prediction_xgb <- model_predict(features,
-                               train_data,
-                               test_data,
-                               model_type = "xgb")
-
-prediction_lbm <- model_predict(features,
-                               train_data,
-                               test_data,
-                               model_type = "lbm")
-
-prediction_rf[2] / prediction_xgb[2] / prediction_lbm[2]
+train_and_plot <- function() {
+  data <- get_stock_data()
+  features <- generate_features(data) %>% 
+    mutate(
+      direction = factor(ifelse(returns > 0, 1, 0))
+    )
+  
+  train_data <- features %>% 
+    filter(date <= as.Date("2023-1-1"))
+  test_data <- features %>% 
+    filter(date > as.Date("2023-1-1"))
+  
+  prediction_rf <- model_predict(features,
+                                 train_data,
+                                 test_data,
+                                 model_type = "rf")
+  
+  
+  prediction_xgb <- model_predict(features,
+                                 train_data,
+                                 test_data,
+                                 model_type = "xgb")
+  
+  prediction_lbm <- model_predict(features,
+                                 train_data,
+                                 test_data,
+                                 model_type = "lbm")
+  
+  (prediction_rf[[2]] + ggtitle("Random Forest")) /
+    (prediction_xgb[[2]] + ggtitle("XGBoost")) /
+    (prediction_lbm[[2]] + ggtitle("LightGBM"))
+}
